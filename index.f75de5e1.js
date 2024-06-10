7d343cef7a725b764c5f016bb6370b0798f83e3d
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-06-10 14:37:35 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+"use strict";
+const thead = document.querySelector("thead");
+const tbody = document.querySelector("tbody");
+const theadCells = [
+    ...thead.rows[0].cells
+];
+const tbodyRows = [
+    ...tbody.rows
+];
+const sortTable = (columnIndex)=>{
+    tbodyRows.sort((a, b)=>a.cells[columnIndex].textContent.trim().localeCompare(b.cells[columnIndex].textContent.trim(), "en", {
+            numeric: true
+        })).forEach((row)=>tbody.appendChild(row));
+};
+theadCells.forEach((cell, index)=>{
+    cell.onclick = ()=>sortTable(index);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
